4f6efe87b4b5a7c7bb4da9869f907aab7e9f9aa7
Tawnee Allport <allprt89@gmail.com>
2020-10-20 21:40:39 -0600

    move themetoggle to header

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Link } from "gatsby"
 import NavLinks from '@components/navLinks'
 import { Hamburger, Times, Logo } from '@images/icons'
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const Header = () => {
   const [isOpen, toggleNav] = useState(false)
@@ -9,6 +10,8 @@ const Header = () => {
   return (
     <header className="header">
       <div className="wrapper header__nav-container">
+
+      
   
         <Link to="/" className="header__logo">
           <Logo />
@@ -30,6 +33,19 @@ const Header = () => {
         <nav className={isOpen ? 'header__nav open' : 'header__nav'}>
           <NavLinks specialClass='header__nav-list' />
         </nav>
+
+        <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <label>
+            <input
+              type="checkbox"
+              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+              checked={theme === 'dark'}
+            />{' '}
+            Dark mode
+          </label>
+        )}
+      </ThemeToggler>
   
       </div>
     </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,26 +4,14 @@ import PropTypes from "prop-types"
 import Header from "@components/header"
 import Footer from "@components/footer"
 // import CookieBanner from "@components/CookieBanner"
-import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import "@styles/styles.scss"
 
 const Layout = ({ children }) => (
   <>
-      <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-          <label>
-            <input
-              type="checkbox"
-              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-              checked={theme === 'dark'}
-            />{' '}
-            Dark mode
-          </label>
-        )}
-      </ThemeToggler>
+    <Header />
+      
     <main>{children}</main>
 
-    {/* <Header /> */}
     {/* <Footer /> */}
     {/* <CookieBanner /> */}
   </>
