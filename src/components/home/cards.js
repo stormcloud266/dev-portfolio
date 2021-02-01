137e1bca2f61ef005ee80d0afc0d7ad881a9749d
Tawnee <allprt89@gmail.com>
2021-02-01 14:01:33 -0700

    adjust gradients

--- src/components/home/cards.js
+++ src/components/home/cards.js
@@ -43,7 +43,15 @@ const Cards = () => (
 export default Cards
 
 const CardsContainer = styled.div`
-  background-image: linear-gradient(120deg, #b1ede8, #4de6da);
+  /* background-image: linear-gradient(120deg, #b1ede8, #4de6da); */
+  background-image: linear-gradient(
+    to right bottom,
+    #f784ae,
+    #d4a5ec,
+    #a1c6ff,
+    #90defb,
+    #b1ede8
+  );
 `
 
 const CardsInner = styled(Container)`
@@ -71,7 +79,8 @@ const Card = styled.div`
     `}
 
   svg {
-    stroke: ${props => (props.theme.isDark ? "#b1ede8" : "#4de6da")};
+    stroke: ${props =>
+      props.theme.isDark ? "var(--color-blue)" : "var(--color-blue)"};
     margin-bottom: var(--s-6);
   }
 
