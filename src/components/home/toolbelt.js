0f8d5f10c674d53fadbec8463e2341a3202cf8af
Tawnee <allprt89@gmail.com>
2022-12-31 08:34:34 -0700

    update toolbelt bio and tech list

--- src/components/home/toolbelt.js
+++ src/components/home/toolbelt.js
@@ -6,17 +6,20 @@ import dots from "@images/dots.svg"
 import { ArrowRight } from "../../assets/images/icons"
 
 const list = [
-  "React",
   "GatsbyJS",
   "NextJS",
   "RedwoodJS",
   "Typescript",
   "GraphQL",
+  "NodeJS",
+  "Web3",
+  "Solidity",
   "SCSS",
   "PostCSS",
   "Tailwind",
   "Styled Components",
   "Framer Motion",
+  "Postgres",
 ]
 
 const Toolbelt = () => {
@@ -31,13 +34,22 @@ const Toolbelt = () => {
           <FlexItem>
             <Fade>
               <Text>
-                Past. Present. Future. Stack. Honed in on CSS and semantic HTML.
+                I started my journey as a professional front-end developer by
+                doing freelance work for SMB clients. This consisted of
+                designing and building static websites and blogs using GatsbyJS,
+                several headless content management systems, SCSS, and Figma.
               </Text>
               <Text>
-                Moved to React mostly focusing on static sites and blogs for SMB
-                clients.
+                During this time I began dabbling the web3 space doing freelance
+                and contract work using NextJS, React Native, Framer Motion, and
+                the OpenSea API.
               </Text>
-              <Text>Fontend dev RedwoodJS, GraphQL, Tailwind</Text>
+              <Text>
+                Currently, I work at Infinity Keys building unique digital
+                challenges and interfacing with smart contracts. I've built API
+                routes, implemented auth solutions, published an NPM package,
+                and used GraphQL to interact with Postgres.
+              </Text>
             </Fade>
           </FlexItem>
           <FlexItem>
@@ -87,20 +99,25 @@ const FlexList = styled.div`
   display: flex;
   justify-content: center;
 `
-const List = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  column-gap: 2rem;
-  row-gap: 1.2rem;
-`
 
 const Text = styled.p`
   margin-top: var(--s-4);
 `
 
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 2rem;
+  row-gap: 1.2rem;
+  margin-top: var(--s-4);
+`
 const ListItem = styled.li`
   display: flex;
   align-items: center;
+  font-family: var(--font-header);
+  font-weight: 900;
+  color: var(--color-text-primary);
+
   svg {
     color: var(--color-cta);
     margin-right: 0.4rem;
